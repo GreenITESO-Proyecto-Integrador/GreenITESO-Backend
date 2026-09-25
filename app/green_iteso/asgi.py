771407c19355fb5a983b743c7ff726073e20a098
Diego Alonso Gomez Martin del Campo <144501409+ANAVARIUS@users.noreply.github.com>
2026-09-25 08:43:54 -0600

    feat: implement ASGI server support and update Makefile commands (#103)

--- app/green_iteso/asgi.py
+++ app/green_iteso/asgi.py
@@ -4,5 +4,15 @@ import os
 
 from django.core.asgi import get_asgi_application
 
+# import notifications.routing
+
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "green_iteso.settings")
 application = get_asgi_application()
+# application = ProtocolTypeRouter({
+#     "http": django_asgi_app,
+#     "websocket": AuthMiddlewareStack(
+#         URLRouter(
+#             notifications.routing.websocket_urlpatterns
+#         )
+#     ),
+# })
